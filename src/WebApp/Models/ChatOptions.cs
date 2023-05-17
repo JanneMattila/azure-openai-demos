b2353c946435e7db0611f18348c76b08624b56af
Janne Mattila <janne.mattila@hotmail.fi>
2023-05-17 13:00:29 +0300

    Add chat service implementation

--- src/WebApp/Models/ChatOptions.cs
+++ src/WebApp/Models/ChatOptions.cs
@@ -2,4 +2,8 @@
 
 public class ChatOptions
 {
+    public string Endpoint { get; set; } = string.Empty;
+    public string ModelName { get; set; } = string.Empty;
+    public string Prompt { get; set; } = string.Empty;
+    public bool Disabled { get; set; } = false;
 }
--- src/WebApp/Services/ChatService.cs
+++ src/WebApp/Services/ChatService.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Extensions.Options;
 using WebApp.Models;
 
 namespace WebApp.Services;
@@ -7,15 +9,47 @@ public class ChatService : IChatService
 {
     private readonly ILogger _logger;
     private readonly ChatOptions _options;
+    private readonly OpenAIClient _client;
+    private List<ChatMessage> _chats = new List<ChatMessage>();
+    private ChatCompletionsOptions _chatCompletionsOptions;
 
     public ChatService(ILoggerFactory loggerFactory, IOptions<ChatOptions> options)
     {
         _logger = loggerFactory.CreateLogger<ChatService>();
         _options = options.Value;
+
+        _client = new OpenAIClient(new Uri(_options.Endpoint), new DefaultAzureCredential());
+
+        _chatCompletionsOptions = new ChatCompletionsOptions
+        {
+            ChoicesPerPrompt = 1,
+            MaxTokens = 800,
+            Temperature = 0.7f,
+            FrequencyPenalty = 0.0f,
+            PresencePenalty = 0.0f,
+            NucleusSamplingFactor = 0.95f // Top P
+        };
+
+        _chatCompletionsOptions.Messages.Add(new ChatMessage(ChatRole.System, _options.Prompt));
     }
 
     public async Task<string> GetResponseAsync(string text)
     {
-        return await Task.FromResult(text);
+        if (_options.Disabled)
+        {
+            await Task.Delay(1000);
+            return $"Automated response to: {text}";
+        }
+
+        while (_chatCompletionsOptions.Messages.Count > 20) _chatCompletionsOptions.Messages.RemoveAt(0);
+
+        _chatCompletionsOptions.Messages.Add(new ChatMessage(ChatRole.User, text));
+
+
+        var response = await _client.GetChatCompletionsAsync(_options.ModelName, _chatCompletionsOptions);
+
+        var chatResponse = response.Value.Choices.First().Message.Content;
+        _chatCompletionsOptions.Messages.Add(new ChatMessage(ChatRole.Assistant, chatResponse));
+        return chatResponse;
     }
 }
