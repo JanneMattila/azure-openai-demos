6a062bc250a86f44fd0c95582c582bbfe6b41734
Janne Mattila <janne.mattila@hotmail.fi>
2023-05-17 10:21:26 +0300

    Add API for chat

---
+++ src/WebApp/Controllers/ChatController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ChatController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Post([FromBody] ChatModel request)
+    {
+        return Ok(new ChatModel()
+        {
+            Text = $"Received message: {request.Text}"
+        });
+    }
+}
---
+++ src/WebApp/Models/ChatModel.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApp.Models;
+
+public class ChatModel
+{
+    public string Text { get; set; } = string.Empty;
+}
